fb2fb8ebee12354510ccff5aa38c29c32b34cd8b
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-07-20 19:08:24 -0600

    Añadiendo grafico de dona

--- controllers/utils/components.js
+++ controllers/utils/components.js
@@ -382,3 +382,37 @@ const areaGraph = (canvas, xAxis, yAxis, legend, title) => {
         }
     });
 }
+
+/*
+*   Función para generar un gráfico de dona. Requiere la librería chart.js para funcionar.
+*   Parámetros: canvas (identificador de la etiqueta canvas), legends (valores para las etiquetas), values (valores de los datos) y title (título del gráfico).
+*   Retorno: ninguno.
+*/
+const doughnutGraph = (canvas, legends, values, title) => {
+    // Se declara un arreglo para guardar códigos de colores en formato hexadecimal.
+    let colors = [];
+    // Se generan códigos hexadecimales de 6 cifras de acuerdo con el número de datos a mostrar y se agregan al arreglo.
+    values.forEach(() => {
+        colors.push('#' + (Math.random().toString(16)).substring(2, 8));
+    });
+    // Se crea una instancia para generar el gráfico con los datos recibidos.
+    new Chart(document.getElementById(canvas), {
+        type: 'doughnut',
+        data: {
+            labels: legends,
+            datasets: [{
+                data: values,
+                backgroundColor: colors
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: title
+                }
+            }
+        }
+    });
+}
+
